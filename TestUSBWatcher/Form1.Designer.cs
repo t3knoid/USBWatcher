7a42257cee50c6605a7d0a1f7dc8c5c5e73f001a
Frank Refol <frefol@strozfriedberg.com>
2022-02-06 15:45:23 -0500

    Initial commit

---
+++ TestUSBWatcher/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace TestUSBWatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(35, 40);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(721, 353);
+            this.textBox1.TabIndex = 0;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ TestUSBWatcher/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.Reflection;
+
+namespace TestUSBWatcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void DeviceInsertedEvent(object sender, EventArrivedEventArgs e)
+        {
+            textBox1.BeginInvoke(new Action(() => { textBox1.AppendText("********** USB plugged in **********"); }));
+            textBox1.BeginInvoke(new Action(() => { textBox1.AppendText(Environment.NewLine); }));
+
+            //string driveName = e.NewEvent.Properties["DriveName"].Value.ToString();
+            //textBox1.BeginInvoke(new Action(() => { textBox1.AppendText("DriveName = " + driveName); }));
+            //textBox1.BeginInvoke(new Action(() => { textBox1.AppendText(Environment.NewLine); }));
+            
+            IDictionary<string, string> properties = new Dictionary<string, string>();
+
+            foreach (var property in e.NewEvent.Properties)
+            {
+                if (property.Value != null)
+                {
+                    properties[property.Name] = property.Value.ToString();
+                    textBox1.BeginInvoke(new Action(() => { textBox1.AppendText(property.Name + " = " + property.Value); }));
+                    textBox1.BeginInvoke(new Action(() => { textBox1.AppendText(Environment.NewLine); }));
+                }
+            }
+
+            ManagementBaseObject instance = (ManagementBaseObject)e.NewEvent["TargetInstance"];
+            foreach (var property in instance.Properties)
+            {
+                if (property.Value != null)
+                {
+                    properties[property.Name] = property.Value.ToString();
+                    textBox1.BeginInvoke(new Action(() => { textBox1.AppendText(property.Name + " = " + property.Value); }));
+                    textBox1.BeginInvoke(new Action(() => { textBox1.AppendText(Environment.NewLine); }));
+                }
+            }
+
+            textBox1.BeginInvoke(new Action(() => { textBox1.AppendText("********** Drive Properties **********"); }));
+            textBox1.BeginInvoke(new Action(() => { textBox1.AppendText(Environment.NewLine); }));
+            System.Threading.Thread.Sleep(3000);
+            SystemUSBDrives systemUSBDrives = new SystemUSBDrives("");
+            var USBDrivesEnum = systemUSBDrives.GetUSBDrivesInfo("", "", "");
+            
+            foreach (SystemUSBDrives.USBDriveInfo info in USBDrivesEnum)
+            {
+                PropertyInfo[] p = typeof(SystemUSBDrives.USBDriveInfo).GetProperties();
+                foreach (PropertyInfo property in p)
+                {
+                    textBox1.BeginInvoke(new Action(() => { textBox1.AppendText(property.Name + " = " + property.GetValue(info, null)); }));
+                    textBox1.BeginInvoke(new Action(() => { textBox1.AppendText(Environment.NewLine); }));
+                }
+            }
+
+        }
+
+        private void DeviceRemovedEvent(object sender, EventArrivedEventArgs e)
+        {
+            textBox1.BeginInvoke(new Action(() => { textBox1.AppendText("********** USB plugged out **********"); }));
+            textBox1.BeginInvoke(new Action(() => { textBox1.AppendText(Environment.NewLine); }));
+
+            //string driveName = e.NewEvent.Properties["DriveName"].Value.ToString();
+            //textBox1.BeginInvoke(new Action(() => { textBox1.AppendText("DriveName = " + driveName); }));
+            //textBox1.BeginInvoke(new Action(() => { textBox1.AppendText(Environment.NewLine); }));
+
+            ManagementBaseObject instance = (ManagementBaseObject)e.NewEvent["TargetInstance"];
+            foreach (var property in instance.Properties)
+            {
+                textBox1.BeginInvoke(new Action(() => { textBox1.AppendText(property.Name + " = " + property.Value); }));
+                textBox1.BeginInvoke(new Action(() => { textBox1.AppendText(Environment.NewLine); }));
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.IsBusy != true)
+            {
+                // Start the asynchronous operation.
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //WqlEventQuery insertQuery = new WqlEventQuery("SELECT * FROM Win32_DeviceChangeEvent WHERE EventType = 2");
+            WqlEventQuery insertQuery = new WqlEventQuery("SELECT * FROM __InstanceCreationEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_USBHub'");
+
+            ManagementEventWatcher insertWatcher = new ManagementEventWatcher(insertQuery);
+            insertWatcher.EventArrived += new EventArrivedEventHandler(DeviceInsertedEvent);
+            insertWatcher.Start();
+
+            //WqlEventQuery removeQuery = new WqlEventQuery("SELECT * FROM Win32_DeviceChangeEvent WHERE EventType = 3");
+            WqlEventQuery removeQuery = new WqlEventQuery("SELECT * FROM __InstanceDeletionEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_USBHub'");
+            ManagementEventWatcher removeWatcher = new ManagementEventWatcher(removeQuery);
+            removeWatcher.EventArrived += new EventArrivedEventHandler(DeviceRemovedEvent);
+            removeWatcher.Start();
+
+            // Do something while waiting for events
+            System.Threading.Thread.Sleep(20000000);
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("Done");
+        }
+    }
+}
